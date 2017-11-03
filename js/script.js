5a3e25562ca4313c348d897170514b3452fc20dd
jimmybnguyen <jimmyn9@uw.edu>
2017-11-02 21:07:06 -0700

    update sketch file and gps test

--- js/script.js
+++ js/script.js
@@ -2,7 +2,5 @@ window.onload = function() {
   var startPos;
   navigator.geolocation.getCurrentPosition(function(position) {
     startPos = position;
-    document.getElementById('startLat').innerHTML = startPos.coords.latitude;
-    document.getElementById('startLon').innerHTML = startPos.coords.longitude;
   });
 };
