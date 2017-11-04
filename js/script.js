39efc12b7eb6329bcb3c5cca37bb2e7cfca126de
jimmybnguyen <jimmyn9@uw.edu>
2017-11-04 15:31:13 -0700

    Add json file

--- js/script.js
+++ js/script.js
@@ -4,3 +4,7 @@ window.onload = function() {
     startPos = position;
   });
 };
+
+$.getJSON("items.json", function(json) {
+  console.log(json); // this will show the info it in firebug console
+});
