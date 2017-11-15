eede070b7494c4832d9f81c8fbee40d1f6307802
jimmybnguyen <jimmyn9@uw.edu>
2017-11-14 18:00:06 -0800

    placeholder hero img and footer

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
+
+/* get geolocation
 window.onload = function() {
   var startPos;
   navigator.geolocation.getCurrentPosition(function(position) {
     startPos = position;
   });
 };
+*/
+var noods;
 
-$.getJSON("items.json", function(json) {
-  console.log(json); // this will show the info it in firebug console
+$.getJSON("js/items.json", function(json) { // this will show the info it in firebug console
+  noods = json;
+  console.log(noods);
 });
